c21407ea07c960ed7d001836de474d095db93954
Pablo Reis <pablo.reis@wunderman.com>
2021-02-27 13:57:48 -0300

    quotes-list

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-app class="app">
+    <h1>Quotes</h1>
+    <Quotes :quotes="quoteInfo" v-if="quoteInfo.length"
+      @removingQuote="updateQuotes" />
+
+    <div class="buttons-container">
+      <button class="buttons" @click="getQuote">Request Quote</button>
+      <button class="buttons" @click="removeAllQuotes">Remove Quotes</button>
+    </div>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import {
+  Component,
+  Vue,
+} from 'vue-property-decorator';
+import axios from 'axios';
+
+import Quotes from './components/Quotes.vue';
+
+import { IQuote } from './settings/types';
 
 @Component({
   components: {
-    HelloWorld,
+    Quotes,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private quoteInfo: IQuote[] = [];
+
+  getQuote(): void {
+    axios.get<IQuote>('https://api.kanye.rest').then((res) => {
+      this.quoteInfo.push(res.data);
+    });
+  }
+
+  updateQuotes(newQuotesList: IQuote[]): void {
+    this.quoteInfo = newQuotesList;
+  }
+
+  removeAllQuotes(): void {
+    this.quoteInfo = [];
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body, html {
+  margin: 0;
+  padding: 0;
+}
+
+.app {
+  width: 600px;
+  margin: 0 auto;
+  display: flex;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.buttons-container {
+  display: flex;
+  justify-content: center;
+}
+
+.buttons-container button {
+  background: #000;
+  color: #fff;
+  padding: 5px;
+  border-radius: 4px;
+}
+
+.buttons-container button:first-child {
+  margin-right: 10px;
 }
 </style>
---
+++ src/components/Quotes.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="quotes">
+    <ul>
+      <li v-for="(quote, i) in quotesList" :key="quote.quote">
+        <p>{{ quote.quote }} <button @click="removeQuote(i)">X</button></p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Emit,
+  Vue,
+} from 'vue-property-decorator';
+
+import { IQuote } from '../settings/types';
+
+@Component
+export default class Quotes extends Vue {
+  @Prop({ required: true, type: Array }) public quotes: IQuote[];
+
+  private quotesList: IQuote[] = this.quotes;
+
+  @Emit('removingQuote')
+  removeQuote(i: number) {
+    this.quotesList.splice(i, 1);
+
+    return this.quotesList;
+  }
+}
+</script>
+
+<style>
+.quotes ul {
+  list-style: none;
+}
+
+.quotes ul li button {
+  background: #000;
+  color: #fff;
+  padding: 5px;
+  border-radius: 4px;
+}
+</style>
